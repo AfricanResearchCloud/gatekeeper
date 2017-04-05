87b546c3b6a1dcdc1521097d2caeef287015900d
Stefan Coetzee <chrysalis69@gmail.com>
2017-04-05 14:05:38 +0200

    Converted openstack to object

--- onboarding/openstack.py
+++ onboarding/openstack.py
@@ -4,42 +4,46 @@ from keystoneauth1 import session
 from keystoneclient.v3 import client
 from keystoneauth1.exceptions.http import NotFound
 
-OS_AUTHURL = getattr(settings, 'OS_AUTHURL')
-OS_USERNAME = getattr(settings, 'OS_USERNAME')
-OS_PASSWORD = getattr(settings, 'OS_PASSWORD')
-OS_PROJECT = getattr(settings, 'OS_PROJECT')
-OS_USER_DOMAIN = getattr(settings, 'OS_USER_DOMAIN')
-OS_PROJECT_DOMAIN = getattr(settings, 'OS_PROJECT_DOMAIN')
 
-TERMS_ROLE = getattr(settings, 'TERMS_ROLE')
-TERMS_DOMAIN = getattr(settings, 'TERMS_DOMAIN')
 
-USERS_DOMAIN = getattr(settings, 'USERS_DOMAIN')
+class Openstack(object):
+    def __init__(self):
+        self._OS_AUTHURL = getattr(settings, 'OS_AUTHURL')
+        self._OS_USERNAME = getattr(settings, 'OS_USERNAME')
+        self._OS_PASSWORD = getattr(settings, 'OS_PASSWORD')
+        self._OS_PROJECT = getattr(settings, 'OS_PROJECT')
+        self._OS_USER_DOMAIN = getattr(settings, 'OS_USER_DOMAIN')
+        self._OS_PROJECT_DOMAIN = getattr(settings, 'OS_PROJECT_DOMAIN')
 
-TRIAL_PROJECT_DOMAIN = getattr(settings, 'TRIAL_PROJECT_DOMAIN')
-TRIAL_PROJECT_PREFIX = getattr(settings, 'TRIAL_PROJECT_PREFIX')
+        self._TERMS_ROLE = getattr(settings, 'TERMS_ROLE')
+        self._TERMS_DOMAIN = getattr(settings, 'TERMS_DOMAIN')
 
-class Openstack():
-    def get_keystone_client():
-        auth = v3.Password(auth_url=OS_AUTHURL, username=OS_USERNAME, password=OS_PASSWORD,
-                            project_name=OS_PROJECT, user_domain_name=OS_USER_DOMAIN, project_domain_name=OS_PROJECT_DOMAIN)
+        self._USERS_DOMAIN = getattr(settings, 'USERS_DOMAIN')
+
+        self._TRIAL_PROJECT_DOMAIN = getattr(settings, 'TRIAL_PROJECT_DOMAIN')
+        self._TRIAL_PROJECT_PREFIX = getattr(settings, 'TRIAL_PROJECT_PREFIX')
+        self.keystone = self._get_keystone_client()
+
+    def _get_keystone_client(self):
+        auth = v3.Password(auth_url=self._OS_AUTHURL, username=self._OS_USERNAME, password=self._OS_PASSWORD,
+                            project_name=self._OS_PROJECT, user_domain_name=self._OS_USER_DOMAIN, project_domain_name=self._OS_PROJECT_DOMAIN)
         sess = session.Session(auth=auth)
         return client.Client(session=sess)
 
-    def get_user_list(keystone, user):
-        return keystone.users.list(name=user, domain=USERS_DOMAIN)
+    def get_user_list(self, user):
+        return self.keystone.users.list(name=user, domain=self._USERS_DOMAIN)
 
-    def is_registered_user(list):
+    def is_registered_user(self, list):
         return len(list) == 1
 
-    def has_terms(keystone, user):
-        role = keystone.roles.list(domain=TERMS_DOMAIN, name=TERMS_ROLE)[0]
+    def has_terms(self, user):
+        role = self.keystone.roles.list(domain=self._TERMS_DOMAIN, name=self._TERMS_ROLE)[0]
         try:
-            return keystone.roles.check(role, user=user, domain='default')
+            return self.keystone.roles.check(role, user=user, domain='default')
         except NotFound:
             return False
 
     def trial_project_exist(keystone, user):
-        trial_project_name = "%s%s" % (TRIAL_PROJECT_PREFIX, user.name)
-        trial_projects_list = keystone.projects.list(domain=TRIAL_PROJECT_DOMAIN, name=trial_project_name)
+        trial_project_name = "%s%s" % (self._TRIAL_PROJECT_PREFIX, user.name)
+        trial_projects_list = self.keystone.projects.list(domain=self._TRIAL_PROJECT_DOMAIN, name=trial_project_name)
         return (len(trial_projects_list) == 1) and (trial_projects_list[0].is_trial == 'True' if hasattr(trial_projects_list[0], 'is_trial') else False)
--- onboarding/views.py
+++ onboarding/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import os;
+from django.conf import settings
+from onboarding.openstack import Openstack
+import os
 
+SHIB_USER_ATTRIBUTE = getattr(settings, 'SHIB_USER_ATTRIBUTE')
 
 def index(request):
-
+    openstack_client = Openstack()
     # Check for Shibboleth Session
-    
-
+    username = request.META.get(SHIB_USER_ATTRIBUTE)
     # Query Keystone for user
+
     return HttpResponse("Hello, world. You're at the polls index.")
